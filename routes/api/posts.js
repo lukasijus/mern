ee96dcba36141346d8dbfb1a8282613bfb7e6f5c
Lukas Rimkus <lukasrimkuslg@gmail.com>
2020-06-16 23:46:32 +0300

    tuesday-update

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+// @route   GET api/posts/test
+// @desc    Tests post route
+// @access  Public
+router.get('/test', (req, res) => res.json({ msg: 'Posts Works' }));
 
-// @route       GET api/posts
-// @desc        Test route
-// @access      Public
-router.get('/', (req,res) => {
-    res.send('Post route')
-})
-
-module.exports = router
+module.exports = router;
