005c3ac6ad4892376f6482abc30e4fb963f13fd7
Lukas Rimkus <lukasrimkuslg@gmail.com>
2020-06-27 22:33:58 +0200

    update-mern-2020-06-27

--- client/src/App.js
+++ client/src/App.js
@@ -6,21 +6,30 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import './App.css';
 
+// Redux
+import {Provider} from 'react-redux';
+import store from './store';
+
+
 const App = () => {
-  return (
-    <Router>
-      <Fragment>
-      <Navbar></Navbar>
-      <Route exact path='/' component={Landing}/>
-      <section className="container">
-        <Switch>
-          <Route exact path="/register" component={Register}/>
-          <Route exact path="/login" component={Login}/> 
-        </Switch>
-      </section>
-    </Fragment>
-    </Router>
-  );
+  return(
+      <Provider store={store}>
+            <Router>
+              <Fragment>
+              <Navbar></Navbar>
+              <Route exact path='/' component={Landing}/>
+              <section className="container">
+                <Switch>
+                  <Route exact path="/register" component={Register}/>
+                  <Route exact path="/login" component={Login}/> 
+                </Switch>
+              </section>
+            </Fragment>
+          </Router>
+
+      </Provider>   
+  )
+    
 }
 
 export default App;
